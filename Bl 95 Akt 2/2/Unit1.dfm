88d308d404cf5e2bc5d020e94adfd5fa665ec463
Danieldan5152 <danieldan5152@gmail.com>
2017-03-05 20:44:37 +0200

    Broken SQL

---
+++ Bl 95 Akt 2/2/Unit1.dfm
@@ -0,0 +1,70 @@
+object frmEdwardShop: TfrmEdwardShop
+  Left = 0
+  Top = 0
+  Caption = 'frmEdwardShop'
+  ClientHeight = 636
+  ClientWidth = 728
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object dbgShow: TDBGrid
+    Left = 8
+    Top = 8
+    Width = 649
+    Height = 433
+    DataSource = dmEdward.dsrEdward
+    TabOrder = 0
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -11
+    TitleFont.Name = 'Tahoma'
+    TitleFont.Style = []
+  end
+  object rgpItems: TRadioGroup
+    Left = 24
+    Top = 450
+    Width = 225
+    Height = 178
+    Caption = 'rgpItems'
+    Items.Strings = (
+      'Kg Products'
+      'Cape Supplier'
+      'Baby Food'
+      'KOO'
+      'Bubblegum')
+    TabOrder = 1
+  end
+  object btnShow: TButton
+    Left = 296
+    Top = 488
+    Width = 75
+    Height = 25
+    Caption = 'btnShow'
+    TabOrder = 2
+    OnClick = btnShowClick
+  end
+  object bmbRetry: TBitBtn
+    Left = 392
+    Top = 488
+    Width = 75
+    Height = 25
+    Kind = bkRetry
+    NumGlyphs = 2
+    TabOrder = 3
+  end
+  object bmbClose: TBitBtn
+    Left = 488
+    Top = 488
+    Width = 75
+    Height = 25
+    Kind = bkClose
+    NumGlyphs = 2
+    TabOrder = 4
+  end
+end
---
+++ Bl 95 Akt 2/2/Unit1.pas
@@ -0,0 +1,115 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Data.DB, StdCtrls, ExtCtrls, Grids, DBGrids, dmEdward_u, Vcl.Buttons;
+
+type
+  TfrmEdwardShop = class(TForm)
+    dbgShow: TDBGrid;
+    rgpItems: TRadioGroup;
+    btnShow: TButton;
+    bmbRetry: TBitBtn;
+    bmbClose: TBitBtn;
+    procedure btnShowClick(Sender: TObject);
+  private
+    procedure Kg_Prod;
+    procedure Cape_Supp;
+    procedure Bab_Foo;
+    procedure Koo;
+    procedure Bubble;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  iItem : integer;
+  frmEdwardShop: TfrmEdwardShop;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmEdwardShop.Kg_Prod;
+begin
+  with dmEdward do
+    begin
+      qryEdward.SQL.clear;
+      qryEdward.SQL.Add('SELECT ProductDesc FROM Item');
+      qryEdward.SQL.Add('WHERE ProductDesc LIKE "Kg%"');
+      qryEdward.Open;
+    end;
+end;
+
+procedure TfrmEdwardShop.Cape_Supp;
+begin
+  with dmEdward do
+    begin
+      qryEdward.SQL.clear;
+      qryEdward.SQL.Add('SELECT ContactNo,FirstNames,Surname FROM Supplier');
+      qryEdward.SQL.Add('WHERE ContactNo LIKE "021%"');
+      qryEdward.Open;
+    end;
+
+end;
+
+procedure TfrmEdwardShop.Bab_Foo;
+begin
+  with dmEdward do
+    begin
+      qryEdward.SQL.clear;
+      qryEdward.SQL.Add('SELECT * FROM Item');
+      qryEdward.SQL.Add('WHERE BrandName = "Nestrum","Nan","Puity"');
+      qryEdward.SQL.Add('WHERE CostPrice <= 50.00');
+      qryEdward.Open;
+    end;
+end;
+
+procedure TfrmEdwardShop.Koo;
+begin
+  with dmEdward do
+    begin
+      qryEdward.SQL.clear;
+      qryEdward.SQL.Add('SELECT BrandName,ProductDesc,CostPrice FROM Item');
+      qryEdward.SQL.Add('WHERE BrandName = "KOO"');
+      qryEdward.SQL.Add('WHERE CostPrice >= 40.00');
+      qryEdward.SQL.Add('WHERE CostPrice <= 60.00');
+      qryEdward.Open;
+    end;
+end;
+
+procedure TfrmEdwardShop.Bubble;
+begin
+  with dmEdward do
+    begin
+      qryEdward.SQL.clear;
+      qryEdward.SQL.Add('SELECT SupplierID,BrandName,ProductDesc FROM Item');
+      qryEdward.SQL.Add('WHERE BrandName = "Chappies"');
+      qryEdward.SQL.Add('WHERE ProductDesc LIKE "bubblegum%"');
+      qryEdward.Open;
+    end;
+end;
+
+procedure TfrmEdwardShop.btnShowClick(Sender: TObject);
+begin
+  iItem := rgpItems.ItemIndex; // what item is selected
+  if iItem = -1 then
+    begin
+      showmessage('Select a item');
+      exit; //stops code!!!!
+    end;
+
+  case iItem of
+    0 : Kg_Prod   ;
+    1 : Cape_Supp ;
+    2 : Bab_Foo   ;
+    3 : KOO       ;
+    4 : Bubble    ;
+  end;
+
+end;
+
+end.
